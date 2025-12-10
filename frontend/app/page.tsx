1901ad4d82ba970023d0ee7e60931a1cdc19ad5f
meherbz <bz24meher@gmail.com>
2025-12-10 18:29:04 +0100

    final

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,35 +3,35 @@ import Card from "../components/card/Card";
 import { getPosts } from "../fetch";
 
 export default async function Page() {
-  const [featuredPosts, Posts] = await Promise.all([
-    await getPosts("filters[IsFeatured][$eq]=true"),
-    await getPosts("filters[IsFeatured][$eq]=false"),
-  ]);
+    const [featuredPosts, Posts] = await Promise.all([
+        await getPosts("filters[IsFeatured][$eq]=true"),
+        await getPosts("filters[IsFeatured][$eq]=false"),
+    ]);
 
-  return (
-    <div className="container pb-80">
-      {featuredPosts.map((featuredPost) => (
-        <Card
-          key={featuredPost.id}
-          label={featuredPost.category.name}
-          title={featuredPost.title}
-          summary={featuredPost.description}
-          href={featuredPost.slug}
-          className="mb-30"
-        />
-      ))}
-      <div className="row">
-        {Posts.map((Post) => (
-          <div className="col col-4" key={Post.id}>
-            <Card
-              label={Post.category.name}
-              title={Post.title}
-              summary={Post.description}
-              href={Post.slug}
-            />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="container pb-80">
+            {featuredPosts.map((featuredPost) => (
+                <Card
+                    key={featuredPost.id}
+                    label={featuredPost.category.name}
+                    title={featuredPost.title}
+                    summary={featuredPost.description}
+                    href={featuredPost.slug}
+                    className="mb-30"
+                />
+            ))}
+            <div className="row">
+                {Posts.map((Post) => (
+                    <div className="col col-4 m-mw-100" key={Post.id}>
+                        <Card
+                            label={Post.category.name}
+                            title={Post.title}
+                            summary={Post.description}
+                            href={Post.slug}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
